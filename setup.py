d2c2b75f4d468d4e8c18da93db5d1ce099729457
Florian Engel <florian.engel@adletec.de>
2016-01-15 23:27:23 +0100

    Bumps version to 0.1.1

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
         name='turboline',
         packages=['turboline'],
-        version='0.1',
+        version='0.1.1',
         description='A simple but powerful vim-like command line for curses based applications.',
         author='Florian Engel',
         author_email='vlow@turbocache3000.de',
         url='https://github.com/vlow/turboline',
-        download_url='https://github.com/vlow/turboline/tarball/0.1',
+        download_url='https://github.com/vlow/turboline/tarball/0.1.1',
         keywords=['command', 'cli', 'curses', 'vim'],
         classifiers=[],
 )
