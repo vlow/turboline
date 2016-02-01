dcefdee238c3e85d15df0570a06721bb5afe025b
Florian Engel <florian.engel@adletec.de>
2016-02-01 21:42:40 +0100

    Adjusts setup.py to new version.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
         author='Florian Engel',
         author_email='vlow@turbocache3000.de',
         url='https://github.com/vlow/turboline',
-        download_url='https://github.com/vlow/turboline/tarball/0.1.2',
+        download_url='https://github.com/vlow/turboline/tarball/0.2.0',
         keywords=['command', 'cli', 'curses', 'vim'],
         classifiers=[],
 )
